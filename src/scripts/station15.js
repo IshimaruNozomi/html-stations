5a02ac059665b6c80938cd3c4fafd029a8e83bd5
Nozomi Ishimaru <s2322096@stu.musashino-u.ac.jp>
2024-05-04 23:16:43 +0900

    HTMLStation15クリア

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,28 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
-  return await result
+  const result = await test();
+  return result.map(buildFullName);
 }
 
 function test() {
-  return
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([
+        { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
+        { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
+      ]);
+    }, 3000);
+  });
 }
 
+function buildFullName(data) {
+  const full_name = `${data.family_name} ${data.first_name}`; // 姓と名を半角スペースで繋げる
+  data.full_name = full_name; // full_nameプロパティをオブジェクトに追加
+  return data; // 変更を加えたオブジェクトを返す
+}
+
+(async () => {
+  const result = await getData();
+  console.log(result);
+})();
